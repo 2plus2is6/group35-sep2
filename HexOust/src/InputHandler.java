460caf08760a3f9fba6cbd2428435431fc16befc
bajpaiCode <ujjwal.bajpai@ucdconnect.ie>
2025-02-22 21:27:16 +0000

    Input Handler Class

--- HexOust/src/InputHandler.java
+++ HexOust/src/InputHandler.java
@@ -1,2 +1,16 @@
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class InputHandler {
+    private Button exitButton; //Stores the exit button
+
+    public InputHandler(Stage stage) {
+        exitButton = new Button("Exit Game"); // The text to be displayed
+
+        exitButton.setOnAction(e -> stage.close()); // The action the button is required to follow upon being clicked
+    }
+
+    public Button getExitButton() {
+        return exitButton;
+    }
 }
