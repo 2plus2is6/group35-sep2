30a385adc16b840df6aa85efd83407b0bd9b52ef
bajpaiCode <ujjwalbajpai2023@gmail.com>
2025-02-22 23:40:25 +0000

    Update InputHandler.java

--- HexOust/src/InputHandler.java
+++ HexOust/src/InputHandler.java
@@ -6,6 +6,15 @@ public class InputHandler {
 
     public InputHandler(Stage stage) {
         exitButton = new Button("Exit Game"); // The text to be displayed
+        //Formatting
+        exitButton.setStyle(
+                "-fx-font-size: 16px; " +
+                        "-fx-background-color: PURPLE; " +
+                        "-fx-text-fill: white; " +
+                        "-fx-padding: 10px 20px; " +
+                        "-fx-border-radius: 5px; " +
+                        "-fx-background-radius: 5px;"
+        );
 
         exitButton.setOnAction(e -> stage.close()); // The action the button is required to follow upon being clicked
     }
